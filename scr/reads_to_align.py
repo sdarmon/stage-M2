246dfaf770040359ad54d990eeb5b6463dc90c6e
Sasha Darmon <sdarmon@umr5558-6fc3s12.univ-lyon1.fr>
2022-02-10 14:57:08 +0100

    correction

--- scr/reads_to_align.py
+++ scr/reads_to_align.py
@@ -2,23 +2,25 @@ import numpy as np
 import sys
 
 
-Arg = sys.argv[1:]
+Arg = sys.argv[:]
 
-if len(Arg) == 0:
-    print("No argument given....")
+if len(Arg) != 4:
+    print("Use : "+Arg[0]+ " input.txt output.fq threshold")
 else:
-    y = []
-    m = 0
-    with open(Arg[0]) as f:
+    seqs = []
+    t = Arg[3]
+    with open(Arg[1],'r') as f:
         for line in f:
             if (len(line)<2):
                 break
             L = line.split('\t')
-            y.append(int(L[1][:-1]))
-            m = max(m,y[-1])
-        x = np.arange(1,m+1,1)
-        Y = [0 for i in range(m)]
-        for el in y:
-            Y[el-1] +=100/len(y)
-        for el in Y:
-            print(el)
+            if (int(L[2][:-1])> t):
+                seqs.append(int(L[1]))
+    with open(Arg[2],'w') as f:
+        compt = 0
+        for seq in seqs:
+            f.write("@SEQ_"+str(compt))
+            compt+=1
+            f.write(seq)
+            f.write("+")
+            f.write("J"*len(seq))
