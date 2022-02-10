800500a10335d6a75b002ddaa3505c9bbaf22fd4
Sasha Darmon <sdarmon@umr5558-6fc3s12.univ-lyon1.fr>
2022-02-10 14:43:16 +0100

    correction

--- scr/plot.py
+++ scr/plot.py
@@ -97,7 +97,7 @@ elif Arg[2] == "top10":
 			break
 		size+=1
 	print(size)
-	print(M)
+	print(Y)
 
 else:
 	print("Wrong format used.\n Use : "+Arg[0]+" output.txt format\n Where format is either 'dot' or 'his', the format of the plot.")
