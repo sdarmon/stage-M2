2a07c79ec16e076c026e5e0a1baadb9f4d22e49a
Sasha Darmon <sdarmon@umr5558-6fc3s12.univ-lyon1.fr>
2022-02-11 14:37:58 +0100

    maj

--- scr/graph.cpp
+++ scr/graph.cpp
@@ -398,7 +398,6 @@ int main(int argc, char** argv)
     if(argc == 5){
         ofstream output;
         output.open(argv[4]);
-        cout << "ici?" << endl;
         printGraphVertices(G,output);
         output.close();
     } else{
