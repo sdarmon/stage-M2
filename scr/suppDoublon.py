6c740e7b76b626948dd9bb399e860cba24b1fdcd
Sasha Darmon <sdarmon@umr5558-6fc3s12.univ-lyon1.fr>
2022-02-14 09:59:32 +0100

    modif

--- scr/suppDoublon.py
+++ scr/suppDoublon.py
@@ -6,6 +6,21 @@ Arg = sys.argv[:]
 
 if len(Arg) != 3:
     print("Use : "+Arg[0]+ " input.txt output.txt")
+elif Arg[1] == "intersectionKiss.txt":
+    with open(Arg[1],'r') as f:
+        with open(Arg[2],'w') as o:
+            oldTarget = ""
+            for line in f:
+                target = line.split("\t")[8]
+                print(target)
+                if oldline == "":
+                    oldTarget = target
+                    o.write(oldline)
+                elif target == oldTarget:
+                    continue
+                else:
+                    o.write(line)
+                    oldTarget = target
 else:
     with open(Arg[1],'r') as f:
         with open(Arg[2],'w') as o:
