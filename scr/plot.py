f6cb1a133b2033b83aaf1c97a6b5c9d86954de40
Sasha Darmon <sdarmon@umr5558-6fc3s12.univ-lyon1.fr>
2022-02-14 14:59:32 +0100

    modif

--- scr/plot.py
+++ scr/plot.py
@@ -5,8 +5,8 @@ import sys
 
 Arg = sys.argv[:]
 
-if len(Arg) <3:
-	print("Wrong input used.\n Use : "+Arg[0]+" output.txt format\n Where format is either 'dot' or 'his', the format of the plot.")
+if len(Arg) not in [3,4]:
+	print("Wrong input used.\n Use : "+Arg[0]+" output.txt format optional_input.txt\n Where format is 'dot', 'his', 'top10' or 'reverse', the format of the plot.")
 
 elif Arg[2] == "dot":
 	y = []
@@ -103,5 +103,62 @@ elif Arg[2] == "top10":
 		size+=1
 	print(size)
 
+elif Arg[2] == "reverse":
+	y = []
+	m = 0
+	ref = []
+	Z = []
+	with open(Arg[3]) as f:
+		for line in f:
+			if (len(line)<2):
+				break
+			L = line.split('\t')
+			ref.append(len(L[1]))
+	with open(Arg[1]) as f:
+		for line in f:
+			if (len(line)<2):
+				break
+			L = line.split('\t')
+			y.append(int(L[2][:-1]))
+			m = max(m,y[-1])
+	x = np.arange(1,m+1,1)
+	Y = [0 for i in range(m)]
+	Z = [0 for i in range(m)]
+	for el in y:
+		Y[el-1] +=100/len(y)
+	for el in ref:
+		Z[el-1] +=100/len(y)
+	xb = []
+	yb = []
+
+	xg = []
+	yg = []
+
+	for i in range(len(x)):
+		if Y[i] == float(0):
+			yb.append(Y[i])
+			xb.append(x[i])
+		else:
+			yg.append(Y[i])
+			xg.append(x[i])
+
+	fig, axs = plt.subplots(3)
+
+	axs[0].plot(xg,yg, 'bo', label = 'Positive Frequency')
+	axs[0].legend()
+	axs[1].plot(xb,yb, 'ro', label = 'Null Frequency')
+	axs[1].legend()
+	axs[2].plot(x,Z, 'yo', label = 'Matching sequencies')
+	axs[2].legend()
+	axs[0].plot(x,Y, 'green')
+	axs[1].plot(x,Y, 'green')
+	axs[2].plot(x,Y, 'green')
+	axs[0].set(ylabel= "Frequencies (in %)")
+	axs[1].set(ylabel= "Frequencies (in %)")
+	axs[2].set(ylabel= "Frequencies of matching sequencies (in %)")
+	plt.xlabel("Sizes of weight")
+	fig.suptitle("Frequencies of the different sizes of weight (Case radius = 200)")
+	plt.show()
+
 else:
-	print("Wrong format used.\n Use : "+Arg[0]+" output.txt format\n Where format is either 'dot' or 'his', the format of the plot.")
+	print("Wrong input used.\n Use : "+Arg[0]+" output.txt format optional_input.txt\n Where format is 'dot', 'his', 'top10' or 'reverse', the format of the plot.")
