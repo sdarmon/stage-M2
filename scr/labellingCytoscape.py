2b25f64d82e7aa6ea11114a8d58266927d5740d3
Sasha Darmon <sdarmon@umr5558-6fc3s12.univ-lyon1.fr>
2022-02-16 15:31:52 +0100

    modif

--- scr/labellingCytoscape.py
+++ scr/labellingCytoscape.py
@@ -5,12 +5,20 @@ import sys
 Arg = sys.argv[:]
 
 if len(Arg) not in [4]:
-    print("Use : "+Arg[0]+ " inputRead.txt selectedReads.txt output.txt")
+    print("Use : "+Arg[0]+ " edges.txt inputRead.txt selectedReads.txt output.txt")
     exit()
 if len(Arg) == 4:
+    node = set()
+    with open(Arg[3],'r') as f:
+        for line in f:
+            if len(line) <2:
+                break
+            L = line.split("\t")
+            node.add(int(L[0]))
+            node.add(int(L[1]))
     with open(Arg[1],'r') as f:
         with open(Arg[2],'r') as read:
-            with open(Arg[3],'w') as o:
+            with open(Arg[4],'w') as o:
                 reads = read.readlines()
                 index = 0
                 for line in f:
@@ -18,7 +26,9 @@ if len(Arg) == 4:
                         break
                     pos = int(line.split('\t')[0])
                     if pos == int(reads[index].split('\t')[0]):
-                        o.write(line[:-1]+'\t1\n')
+                        if pos in node:
+                            o.write(line[:-1]+'\t1\n')
                         index+=1
                     else:
-                        o.write(line[:-1]+'\t0\n')
+                        if pos in node:
+                            o.write(line[:-1]+'\t0\n')
