920e2151465b2cb2853a059b6e591781e9831f92
Sasha Darmon <sdarmon@umr5558-6fc3s12.univ-lyon1.fr>
2022-02-22 11:28:35 +0100

    modif

---
+++ scr/interseq.py
@@ -0,0 +1,35 @@
+# Petit script permettant faire les intersections entre les
+# lignes de deux fichiers
+
+import numpy as np 
+import sys
+
+
+Arg = sys.argv[:]
+
+if len(Arg) not in [4]:
+    print("Use : "+Arg[0]+ " seq1.txt seq2.txt outputPrefix ")
+else:
+    inter = []
+    seq2Not = []
+    with open(Arg[1],'r') as seq1:
+        Sq = seq1.readlines()
+    with open(Arg[2],'r') as seq2:
+        for line in seq2:
+            if line in Sq:
+                inter.append(line)
+                Sq.remove(line)
+            else:
+                seq2Not.append(line)
+
+    with open(Arg[3]+"intersection.txt",'w') as out:
+        for line in inter:
+            out.write(line)
+
+    with open(Arg[3]+"seq1Missing.txt",'w') as out:
+        for line in Sq:
+            out.write(line)
+
+    with open(Arg[3]+"seq2Missing.txt",'w') as out:
+        for line in seq2Not:
+            out.write(line)
