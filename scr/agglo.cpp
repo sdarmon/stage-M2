78491dee456c6696a7649e3cd354ded463c55eef
Sasha Darmon <sdarmon@umr5558-6fc3s12.univ-lyon1.fr>
2022-03-08 16:05:12 +0100

    modif

--- scr/agglo.cpp
+++ scr/agglo.cpp
@@ -15,14 +15,12 @@
 #define MAX 1024
 
 // Permet de déterminer s'il existe un chemin de la composante i à la composante j, et s'il existe, renvoie la distance.
-int chemin(int i, int j, Graph &G, vector<vector<int>> &components, int profondeurMax){
+void chemin(int i, vector<int> &endings, Graph &G, vector<vector<int>> &components, int profondeurMax){
     int step = 1;
     vector<int> comp1;
-    vector<int> comp2;
     vector<Neighbor*> voisin1;
     voisin1.clear();
     comp1.clear();
-    comp2.clear();
 
     //cout << " i: " << i << "\t j: " << j << endl;
     //cout << "Taille comp i: "<< components[i].size() << "\nTaille comp j: "<< components[j].size()<< endl;
@@ -35,20 +33,25 @@ int chemin(int i, int j, Graph &G, vector<vector<int>> &components, int profonde
             }
         }
     }
-    for(int k= 0; k< components[j].size(); k++){
-        if (find(comp1.begin(),comp1.end(),components[j][k]) != comp1.end()){ 
-            return 0;
+    //On regarde si la j-eme composante s'intersecte avec la comp1
+    for (int j = 0; j<components.size(); j++){
+        for(int k= 0; k< components[j].size(); k++){
+            if (find(comp1.begin(),comp1.end(),components[j][k]) != comp1.end()){ 
+                endings[j] = 0;
+                continue;
+            }
         }
-        comp2.push_back(components[j][k]);
     }
 
     int modif = 1;
+    int trouve = 0;
     int size;
     Neighbor* node;
 
     //cout << "Initialisation chemin ok" << endl;
     while (modif && step < profondeurMax) { //Tant qu'un sommet a été rajouté à la couche précédente, on regarde tous les sommets de cette couche là dans le BFS.
         modif = 0;
+        trouve = 0;
         size = voisin1.size();
         for (int k = 0; k<size; ++k){ //On boucle sur les sommets de la couche du BFS uniquement
             node = voisin1.front();
@@ -56,23 +59,32 @@ int chemin(int i, int j, Graph &G, vector<vector<int>> &components, int profonde
             if (find(comp1.begin(),comp1.end(), node->val) != comp1.end()){ // Sommet déjà présent
                 continue;
             }
-            if (find(comp2.begin(),comp2.end(),node->val) != comp2.end()){ // Sommet également présent dans la composante d'arrivée
-                return step;
-            }
-            //Sinon on rajoute ce sommet comme vu et on ajoute ses voisins à traiter dans le BFS pour la prochaine couche
-            comp1.push_back(node->val);
-            modif = 1;
-            for (vector<Neighbor>::iterator it = G.Neighbors(node->val)->begin(); it != G.Neighbors(node->val)->end(); ++it){ 
-                if (it->label[0] != node->label[1]){ 
+            // Sommet également présent dans la composante d'arrivée
+            for (int j = 0; j<components.size(); j++){
+                if (find(components[j].begin(),components[j].end(),node->val) != components[j].end()){ 
+                    if (endings[j] == -1){ //On garde toujours le plus court chemin
+                        endings[j] = step;
+                    }
+                    trouve = 1;
                     continue;
                 }
-                voisin1.push_back(&(*it));
+            }
+            if (not trouve) {
+                //Sinon on rajoute ce sommet comme vu et on ajoute ses voisins à traiter dans le BFS pour la prochaine couche
+                comp1.push_back(node->val);
+                modif = 1;
+                for (vector<Neighbor>::iterator it = G.Neighbors(node->val)->begin(); it != G.Neighbors(node->val)->end(); ++it){ 
+                    if (it->label[0] != node->label[1]){ 
+                        continue;
+                    }
+                    voisin1.push_back(&(*it));
+                }
             }
         }
         step ++; //On passe à la couche suivante
     }
-    //Aucun nouveau sommet n'a été rajouté à traiter... Il n'y a pas de chemin de la composante i à j.
-    return -1;
+    //Aucun nouveau sommet n'a été rajouté à traiter... Il n'y a plus de chemins à visiter...
+    return;
 }
 
 int indexMax(Graph &G, vector<int> &vu_total){
@@ -86,6 +98,15 @@ int indexMax(Graph &G, vector<int> &vu_total){
     }
     return index;
 }
+
+void initVec(vector<int> &vec, int n){
+    vec.clear();
+    for(int i = 0; i<n; i++){
+        vec.push_back(-1);
+    }
+    return;
+}
+
 int main(int argc, char** argv){
     if (argc!=6){
         cout << "Expected use of this program: \n\n\t" <<argv[0] << " file.node file.edges -c value outputPrefixe\n" << endl;
@@ -162,20 +183,21 @@ int main(int argc, char** argv){
     //Maintenant on construit nouveau graphe contracté
     vector<Edge> E2;
     vector<Node> V2;
+    vector<int> endings;
     E2.clear();
     V2.clear();
     int c;
     for (int i = 0; i < components.size(); i++){
         cout << "Calcul des chemins partant de " << i << endl;
+        initVec(endings, components.size());
+        chemin(i,endings,G,components,10);
+
         V2.push_back(Node(i,components[i].size(),""));
+
         for (int j = 0; j< components.size(); j++){
-            if (i == j){
-                continue;
-            }
-            c = chemin(i,j,G,components,10);
-            if (c>=0) {                
+            if (endings[j] >= 0){             
                 char arete[2] = {'N','N'};
-                E2.push_back(Edge(i,j,c,arete));
+                E2.push_back(Edge(i,j,endings[j],arete));
             }
         }
     }
