9ea24b980d237a4c15f168271edec159f6189b40
sasha Darmon <sasha.darmon@ens-lyon.fr>
2022-03-21 16:24:19 +0100

    lancement de chien

--- scr/graph.cpp
+++ scr/graph.cpp
@@ -396,7 +396,7 @@ void read_edge_file( ifstream &edge_file, vector<Edge>& edges ) {
             } else if (compt == 2){
                 strcpy( p, substr.c_str() );
             }  else {
-                break;
+                continue;
             }
             compt++;
         }
