9dec7847627f20d45a1510544f4bc9b039407a1b
sasha Darmon <sasha.darmon@ens-lyon.fr>
2022-03-25 16:04:25 +0100

    ajout fonction de recupSeq

--- scr/recupSeq.py
+++ scr/recupSeq.py
@@ -6,14 +6,14 @@ import sys
 Arg = sys.argv[:]
 
 if len(Arg) not in [5]:
-    print("Use : " + Arg[0] + "prefixCompTe prefixComp nbComponent pos") #Pos = 12 pour le chien
+    print("Use : " + Arg[0] + " prefixCompTe prefixComp nbComponent pos") #Pos = 12 pour le chien
     exit()
 if len(Arg) == 5:
     for i in range(int(Arg[3])):
         seq = set()
         with open(Arg[1]+str(i)+".txt",'r') as f:
             for line in f:
-                l = int(line.split("\t")[Arg[4]][4:])
+                l = int(line.split("\t")[int(Arg[4])][4:])
                 seq.add(l)
         with open(Arg[2]+str(i)+".txt",'r') as f:
             index = 0
