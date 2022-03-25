58ccd170dce088c5389d1da3e7b53440967bfc26
sasha Darmon <sasha.darmon@ens-lyon.fr>
2022-03-25 16:07:07 +0100

    ajout fonction de recupSeq

--- scr/recupSeq.py
+++ scr/recupSeq.py
@@ -15,9 +15,11 @@ if len(Arg) == 5:
             for line in f:
                 l = int(line.split("\t")[int(Arg[4])][4:])
                 seq.add(l)
-        with open(Arg[2]+str(i)+".txt",'r') as f:
+        with open(Arg[2]+str(i)+".fq",'r') as f:
             index = 0
             for line in f:
+                if line[0]==">":
+                    continue
                 if index in seq:
                     print(line[:-1])
                 index+=1
