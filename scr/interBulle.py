d21dfdf1fdd5893d3e9a6888695d050f64711293
sasha Darmon <sasha.darmon@ens-lyon.fr>
2022-04-06 17:23:00 +0200

    interbulle

--- scr/interBulle.py
+++ scr/interBulle.py
@@ -4,32 +4,36 @@ import sys
 
 Arg = sys.argv[:]
 
+
 def reverseC(seq):
-    s=""
+    s = ""
     for el in seq:
-        if el=='A':
-            s='T'+s
-        elif el=='T':
-            s='A'+s
-        elif el=='C':
-            s='G'+s
-        elif el=='G':
-            s='C'+s
-    return(s)
+        if el == 'A':
+            s = 'T' + s
+        elif el == 'T':
+            s = 'A' + s
+        elif el == 'C':
+            s = 'G' + s
+        elif el == 'G':
+            s = 'C' + s
+    return s
+
+
 def maj(sequ):
-    s=""
+    s = ""
     for el in sequ:
         if el == 'a':
-            s=s+'A'
+            s = s + 'A'
         elif el == 'c':
-            s=s+'C'
+            s = s + 'C'
         elif el == 'g':
-            s=s+'G'
+            s = s + 'G'
         elif el == 't':
-            s=s+'T'
+            s = s + 'T'
         else:
-            s=s+el
-    return(s)
+            s = s + el
+    return s
+
 
 if len(Arg) not in [4]:
     print("Use : " + Arg[0] + " dirComp bulle.fa nbComp")
@@ -40,39 +44,39 @@ if len(Arg) == 4:
     comp = [[] for i in range(int(Arg[3]))]
     kmer = dict()
     unitig = dict()
-    k= 41
+    k = 41
     for i in range(int(Arg[3])):
         with open(Arg[1] + "comp" + str(i) + ".txt", 'r') as f:
             if len(line) < 2:
                 continue
             for line in f:
-                L=line.split("\t")
-                kmer[L[1][:k]]=int(L[0])
-                kmer[reverseC([1])[:k]]=int(L[0])
-                unitig[int(L[0])]=L[1]
+                L = line.split("\t")
+                kmer[L[1][:k]] = int(L[0])
+                kmer[reverseC([1])[:k]] = int(L[0])
+                unitig[int(L[0])] = L[1]
                 comp[i].append(int(L[0]))
 
     with open(Arg[2], 'r') as f:
-        upper=0
+        upper = 0
         for line in f:
             if len(line) < 2:
                 continue
             if titre == "":
                 titre = line[:-1]
-                upper=(upper+1)%2
+                upper = (upper + 1) % 2
                 if upper:
                     titreUpper = titre[:-1]
                 else:
                     titreUnder = titre[:-1]
                 continue
             seq = line[:-1]
-            seq=maj(seq)
+            seq = maj(seq)
             comp_possible = [0 for x in range(int(Arg[3]))]
-            for pos in range(len(seq)-k+1):
-                mer=seq[pos,pos+k]
-                L=kmer.get(mer,[])
+            for pos in range(len(seq) - k + 1):
+                mer = seq[pos, pos + k]
+                L = kmer.get(mer, [])
                 for comp in L:
-                    comp_possible[comp]+=1
+                    comp_possible[comp] += 1
             if upper:
                 seqUpper = line[:-1]
                 upperComp = set()
@@ -90,6 +94,7 @@ if len(Arg) == 4:
                     print(seqUpper)
                     print(titreUnder)
                     print(seqUnder)
-                    print("Both found in components:",upperComp&underComp, "\t Only in upper:", upperComp-underComp, "\t Only in under:", underComp-upperComp)
+                    print("Both found in components:", upperComp & underComp, "\t Only in upper:",
+                          upperComp - underComp, "\t Only in under:", underComp - upperComp)
 
             titre = ""
