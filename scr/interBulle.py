97eedb0692155c3ff753d189cd4e52335b4a6b6d
sasha Darmon <sasha.darmon@ens-lyon.fr>
2022-04-06 17:19:12 +0200

    interbulle

--- scr/interBulle.py
+++ scr/interBulle.py
@@ -1,8 +1,36 @@
 #
 
 import sys
+import numpy as np
 
 Arg = sys.argv[:]
+
+def reverseC(seq):
+    s=""
+    for el in seq:
+        if el=='A':
+            s='T'+s
+        elif el=='T':
+            s='A'+s
+        elif el=='C':
+            s='G'+s
+        elif el=='G':
+            s='C'+s
+    return(s)
+def maj(sequ):
+    s=""
+    for el in sequ:
+        if el == 'a':
+            s=s+'A'
+        elif el == 'c':
+            s=s+'C'
+        elif el == 'g':
+            s=s+'G'
+        elif el == 't':
+            s=s+'T'
+        else:
+            s=s+el
+    return(s)
 
 if len(Arg) not in [3]:
     print("Use : " + Arg[0] + "dirComp bulle.fa nbComp")
@@ -11,28 +39,58 @@ if len(Arg) == 3:
     seq = ""
     titre = ""
     comp = [[] for i in range(Arg[3])]
-
+    kmer = dict()
+    unitig = dict()
+    k= 41
     for i in range(Arg[3]):
         with open(Arg[1] + "comp" + str(i) + ".txt", 'r') as f:
             if len(line) < 2:
                 continue
             for line in f:
-                comp[i].append(line)
+                L=line.split("\t")
+                kmer[L[1][:k]]=int(L[0])
+                kmer[reverseC([1])[:k]]=int(L[0])
+                unitig[int(L[0])]=L[1]
+                comp[i].append(int(L[0]))
 
     with open(Arg[2], 'r') as f:
+        upper=0
         for line in f:
             if len(line) < 2:
                 continue
             if titre == "":
-                titre = line
+                titre = line[:-1]
+                upper=(upper+1)%2
+                if upper:
+                    titreUpper = titre[:-1]
+                else:
+                    titreUnder = titre[:-1]
                 continue
-            seq = line
-            for i in range(Arg[3]):
-                if not comp[i]:
-                    continue
-                with open(Arg[2] + "interbulle" + str(i) + ".txt", 'a') as o:
-                    for el in comp[i]:
-                        if el.split("\t")[1] in seq:
-                            o.write(titre)
-                            o.write(el)
+            seq = line[:-1]
+            seq=maj(seq)
+            comp_possible = [0 for x in range(Arg[3])]
+            for pos in range(len(seq)-k+1):
+                mer=seq[pos,pos+k]
+                L=kmer.get(mer,[])
+                for comp in L:
+                    comp_possible[comp]+=1
+            if upper:
+                seqUpper = line[:-1]
+                upperComp = set()
+                for i in range(len(comp_possible)):
+                    if comp_possible[i] != 0:
+                        upperComp.add(i)
+            else:
+                seqUnder = line[:-1]
+                underComp = set()
+                for i in range(len(comp_possible)):
+                    if comp_possible[i] != 0:
+                        underComp.add(i)
+                if len(underComp) != 0 or len(upperComp) != 0:
+                    print(titreUpper)
+                    print(seqUpper)
+                    print(titreUnder)
+                    print(seqUnder)
+                    print("Both found in components:",upperComp&underComp, "\t Only in upper:", upperComp-underComp, "\t Only in under:", underComp-upperComp)
+
             titre = ""
