217a3c028a9e7bd0ffc5cb0e7e7494826d2d23e0
Sasha Darmon <sasha.darmon@ens-lyon.fr>
2022-04-19 11:39:21 +0200

    maj neighborhood

--- scr/neighborhood.cpp
+++ scr/neighborhood.cpp
@@ -17,35 +17,50 @@
 
 
 //Fonction permettant d'enregistrer une composante du graphe `G` dans un fichier
-void save_comp(Graph &G, vector<int> &compo, string outputPrefix, int rang){
+void save_comp(Graph &G, vector<int> &vu, string outputPrefix, int rang){
     ofstream output;
-    output.open(outputPrefix+"/processing/comp"+to_string(rang)+".txt");
-    for (vector<int>::iterator it = compo.begin(); it != compo.end(); it++){
-        output << *it << "\t" << G.Vertices[*it].label << "\t" << G.Vertices[*it].weight  << "\n";
+    output.open(outputPrefix+"/processing/neighbors"+to_string(rang)+".txt");
+    for (int i = 0; i < G.N ; i++){
+        if (vu[i]){
+            output << i << "\t" << G.Vertices[i].label << "\t" << ((vu[i] < 0) ? 0 : vu[i]) << "\t" << G.Vertices[i].weight << endl;
+        }
     }
     return;
 }
 
 int main(int argc, char** argv) {
     int threshold,dis;
+    string prefixOutput;
     if (argc == 4) {
         threshold = 0;
         dis = 1;
-    } else if (argc == 6 and argv[4] == "-d"){
-        int dis = atoi(argv[5]);
-        threshold= 0;
-    }else if (argc == 6 and argv[4] == "-c"){
-        int threshold = atoi(argv[5]);
+    } else if (argc == 6 and argv[4] == "-o"){
+        prefixOutput = argv[5];
+        threshold = 0;
         dis = 1;
-    }else if (argc == 8 and argv[4] == "-c"){
-        int threshold = atoi(argv[5]);
-        dis = atoi(argv[7]);
-    }else if (argc == 8 and argv[4] == "-d"){
-        int threshold = atoi(argv[7]);
+    }else if (argc == 6 and argv[4] == "-c"){
+        prefixOutput = "./";
+        threshold = atoi(argv[5]);
+        dis = 1;
+    }else if (argc == 6 and argv[4] == "-d"){
+        prefixOutput = "./";
+        threshold = 0;
         dis = atoi(argv[5]);
+    }else if (argc == 8 and argv[4] == "-o" and argv[6] == "-d"){
+        prefixOutput = argv[5];
+        dis = atoi(argv[7]);
+        threshold = 0;
+    }else if (argc == 8 and argv[4] == "-c" and argv[6] == "-d"){
+        threshold = atoi(argv[5]);
+        dis = atoi(argv[7]);
+        prefixOutput = "./";
+    }else if (argc == 10 and argv[4] == "-o" and argv[6] == "-c" and argv[8] == "-c"){
+        prefixOutput = argv[5];
+        threshold = atoi(argv[7]);
+        dis = atoi(argv[9]);
     } else {
         cout << "Expected use of this program: \n\n\t" << argv[0]
-             << " file.nodes file.edges node_id [-c value] [-d dis] \n"
+             << " file.nodes file.edges node_id/input.txt [-o prefixOutput] [-c value] [-d dis] \n"
              << "The option c allows to give a threshold to the weight of visited vertices (default 0) and the option d gives the "
                 "depth of neighbor gathered (default 1)" << endl;
         return 0;
@@ -57,7 +72,20 @@ int main(int argc, char** argv) {
 
     char *nodesPath = argv[1];
     char *edgesPath = argv[2];
-    int node_id = atoi(argv[3]);
+    vector<int> nodes_id;
+    nodes_id.clear();
+
+    if (argv[3].find('.') != argv[3].end()){
+        char *idPath =argv[3];
+        ifstream ids(idPath, std::ios::binary);
+        string line;
+        while (getline(ids, line)){
+            nodes_id.push_back(stoi(line));
+        }
+        ids.close();
+    } else {
+        nodes_id.push_back(atoi(argv[3]));
+    }
     ifstream edges(edgesPath, std::ios::binary);
     ifstream nodes(nodesPath, std::ios::binary);
 
@@ -66,47 +94,52 @@ int main(int argc, char** argv) {
 
     Graph G(V, E);
     vector<Neighbor*> aVoir;
-    aVoir.clear();
     vector<int> pronf;
-    pronf.clear();
     Neighbor* node ;
+    int node_id;
     int p ;
-    vector<int> vu(G.N,0);
-    vu[node_id]=-1; //On le marque comme sommet d'entrée
+    vector<int> vu;
+    for(int rang = 0; rang <nodes_id.size(); rang++) {
 
-    //On ajoute les voisins à visiter
-    for (vector<Neighbor>::iterator it = G.Neighbors(node_id)->begin(); it != G.Neighbors(node_id)->end(); ++it) {
-        if (G.Vertices[it->val].weight >= threshold) {
-            aVoir.push_back(&(*it));
-            pronf.push_back(1);
+        aVoir.clear();
+        pronf.clear();
+        vu.clear();
+        for (int i = 0; i<G.N; i++){
+            vu.push_back(0);
         }
-    }
+        node_id = nodes_id.front());
+        nodes_id.erase(nodes_id.begin());
+        vu[node_id] = -1; //On le marque comme sommet d'entrée
 
-    //On fait un BFS
-    while (aVoir.size() != 0){ //Cas de terminaison, on a terminé le BFS
-        node = aVoir.front();
-        aVoir.erase(aVoir.begin());
-        p = pronf.front();
-        pronf.erase(pronf.begin());
-        if (vu[node->val]){ //Cas où le sommet a été vu par le BFS
-            continue;
-        }
-        vu[node->val]=p;
-
-        for (vector<Neighbor>::iterator it = G.Neighbors(node->val)->begin(); it != G.Neighbors(node->val)->end(); ++it){
-            //On boucle sur ses voisins
-            if (G.Vertices[it->val].weight >= threshold and vu[it->val]==0 and p<= dis){
-                //Cas où l'arrêt est bien valide et sommet non vu avant, ce voisin est rajouté dans la file des visites
+        //On ajoute les voisins à visiter
+        for (vector<Neighbor>::iterator it = G.Neighbors(node_id)->begin(); it != G.Neighbors(node_id)->end(); ++it) {
+            if (G.Vertices[it->val].weight >= threshold) {
                 aVoir.push_back(&(*it));
-                pronf.push_back(p+1);
+                pronf.push_back(1);
             }
         }
-    }
 
-    for (int i = 0; i < G.N ; i++){
-        if (vu[i]){
-            cout << i << "\t" << G.Vertices[i].label << "\t" << ((vu[i] < 0) ? 0 : vu[i]) << "\t" << G.Vertices[i].weight << endl;
+        //On fait un BFS
+        while (aVoir.size() != 0) { //Cas de terminaison, on a terminé le BFS
+            node = aVoir.front();
+            aVoir.erase(aVoir.begin());
+            p = pronf.front();
+            pronf.erase(pronf.begin());
+            if (vu[node->val]) { //Cas où le sommet a été vu par le BFS
+                continue;
+            }
+            vu[node->val] = p;
+
+            for (vector<Neighbor>::iterator it = G.Neighbors(node->val)->begin();
+                 it != G.Neighbors(node->val)->end(); ++it) {
+                //On boucle sur ses voisins
+                if (G.Vertices[it->val].weight >= threshold and vu[it->val] == 0 and p <= dis) {
+                    //Cas où l'arrêt est bien valide et sommet non vu avant, ce voisin est rajouté dans la file des visites
+                    aVoir.push_back(&(*it));
+                    pronf.push_back(p + 1);
+                }
+            }
         }
+        save_comp(G, vu, outputPrefix, rang);
     }
-
 }
