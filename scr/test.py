f1830ded49800961ad203ac8ea2cc78e3a3887d8
sasha Darmon <sasha.darmon@ens-lyon.fr>
2022-05-05 10:59:44 +0200

    création compo

--- scr/test.py
+++ scr/test.py
@@ -1,20 +1,8 @@
 import sys
 
 Arg = sys.argv[:]
-def reverseC(seq):
-    s = ""
-    for el in seq:
-        if el == 'A':
-            s = 'T' + s
-        elif el == 'T':
-            s = 'A' + s
-        elif el == 'C':
-            s = 'G' + s
-        elif el == 'G':
-            s = 'C' + s
-    return s
 
 with open(Arg[1],'r') as f:
     for line in f:
-        seq = line[:-1]
-        print(reverseC(seq))
+        print(line)
+        print(line[:-1])
