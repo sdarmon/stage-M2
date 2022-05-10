0103d07d7b0cff6efa594dbd99f8306bb8c75a4d
sasha Darmon <sasha.darmon@ens-lyon.fr>
2022-05-10 13:56:45 +0200

    modif composantes avec créa graph

--- scr/plot.py
+++ scr/plot.py
@@ -83,7 +83,7 @@ elif Arg[2] == "his":
         Y[(10 * y[i]) // (m+1)] +=1
     for i in range(10):
         Y[i] = 100 * Y[i] / len(y)
-    plt.bar(X,Y, color='green')
+    plt.bar(X[1:],Y[1:], color='green')
     plt.ylabel("Frequencies (in %)")
     plt.xlabel("Sizes of weight")
     plt.title("Histogram of the frequencies of the different sizes of weight (Case radius = 200)")
