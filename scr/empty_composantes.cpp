8acf77f4bea149d16a575d4798e7ea871ac9e2cc
Sasha Darmon <sasha.darmon@ens-lyon.fr>
2022-06-04 19:28:33 +0200

    séparation de l'enregistrement des gene

--- scr/empty_composantes.cpp
+++ scr/empty_composantes.cpp
@@ -79,11 +79,11 @@ int main(int argc, char** argv) {
     string line;
     int compt;
     //On boucle sur les composantes
+    cout << "Début des calculs des arêtes des comp " << nbComp<<endl;
     for (int component = 0; component<nbComp; component++){
-        cout << "Pré-calcul pour la composante " << compteurDeBoucle << " de taille " << comp->size() << endl;
         comp->clear();
-        cout << "Pré-calcul pour la composante " << compteurDeBoucle << " de taille " << comp->size() << endl;
         ifstream file(compoPrefix+to_string(component)+".txt", std::ios::binary);
+        cout << "Pré-calcul pour la composante " << compteurDeBoucle << " de taille " << comp->size() << endl;
         while (getline(file, line)){
             compt=stoi(line.substr(0,line.find('\t')));
             comp->push_back(compt);
