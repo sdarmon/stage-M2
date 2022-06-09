bc808b3064f7c5dc9da68cdde759570ffe3129aa
sasha Darmon <sasha.darmon@ens-lyon.fr>
2022-06-09 09:55:11 +0200

    fonction dummy

--- scr/dummy.py
+++ scr/dummy.py
@@ -9,7 +9,7 @@ with open(Arg[1], 'r') as f:
     for line in f:
         step = (step+1)%4
         if step == 0:
-            vu.add(line[:-1])
+            vu.add(line)
             compt+=1
 print("Il y a "+str(len(vu))+" under paths unqiues sur les "+str(compt)+" bulles.")
 with open(Arg[2], 'w') as o:
