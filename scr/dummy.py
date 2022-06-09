754b844cf7c2e5a74935d8d9584e3894fda1e3f0
sasha Darmon <sasha.darmon@ens-lyon.fr>
2022-06-09 09:55:26 +0200

    fonction dummy

--- scr/dummy.py
+++ scr/dummy.py
@@ -11,7 +11,7 @@ with open(Arg[1], 'r') as f:
         if step == 0:
             vu.add(line)
             compt+=1
-print("Il y a "+str(len(vu))+" under paths unqiues sur les "+str(compt)+" bulles.")
+print("Il y a "+str(len(vu))+" under paths uniques sur les "+str(compt)+" bulles.")
 with open(Arg[2], 'w') as o:
     for el in vu:
         o.write(el)
