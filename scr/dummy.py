92c1f596a4fad22ae54b7249d991a46b183b522c
sasha Darmon <sasha.darmon@ens-lyon.fr>
2022-06-09 09:47:01 +0200

    fonction dummy

--- scr/dummy.py
+++ scr/dummy.py
@@ -2,8 +2,15 @@ import sys
 
 Arg = sys.argv[:]
 seq = []
+vu = set()
+compt = 0
 with open(Arg[1], 'r') as f:
-    with open(Arg[2], 'w') as o:
-        for line in f:
-            L=line.split("\t")
-            o.write("\t".join(L[:8]+["".join(L[8:])]))
+    for line in f:
+        step = (step+1)%4
+        if step == 0:
+            vu.add(line[:-1])
+            compt+=1
+print("Il y a "+str(len(vu))+" under paths unqiues sur les "+str(compt)" bulles.")
+with open(Arg[2], 'w') as o:
+    for el in vu:
+        o.write(el)
