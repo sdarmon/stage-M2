ef5f53aabf2dab406a6a09fb614397d6e2ad88b2
sasha Darmon <sasha.darmon@ens-lyon.fr>
2022-06-22 16:38:44 +0200

    modifi test

--- scr/test.py
+++ scr/test.py
@@ -6,6 +6,6 @@ with open(Arg[1], 'r') as f:
     for line in f:
         step = (step+1)%4
         if step == 0:
-            L = line.split("\t")
+            L = line.split("|")
             id = L[0][1:]+"|"+L[1]
             print(id)
