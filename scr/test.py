43a56b28cf6895eac09c6cb77a05ccbda97888e8
sasha Darmon <sasha.darmon@ens-lyon.fr>
2022-06-22 16:42:08 +0200

    modifi test

--- scr/test.py
+++ scr/test.py
@@ -4,6 +4,8 @@ Arg = sys.argv[:]
 step = -1
 with open(Arg[1], 'r') as f:
     for line in f:
+        if line[0] in ['-','_','#']:
+            continue
         step = (step+1)%4
         if step == 0:
             L = line.split("|")
