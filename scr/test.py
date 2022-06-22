a56f106f65596e8d597aec609d63f4986df96fd5
sasha Darmon <sasha.darmon@ens-lyon.fr>
2022-06-22 16:37:12 +0200

    modifi test

--- scr/test.py
+++ scr/test.py
@@ -1,8 +1,11 @@
 import sys
 
 Arg = sys.argv[:]
-
-with open(Arg[1],'r') as f:
+step = -1
+with open(Arg[1], 'r') as f:
     for line in f:
-        print(line)
-        print(line[:-1])
+        step = (step+1)%4
+        if step == 0:
+            L = line.split("\t")
+            id = L[0][1:]+"|"+L[1]
+            print(id)
