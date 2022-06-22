3ea013230075eca0eab22de58c228a4b016a39df
sasha Darmon <sasha.darmon@ens-lyon.fr>
2022-06-22 19:12:55 +0200

    fonction notInSet

---
+++ scr/notInSet.py
@@ -0,0 +1,21 @@
+import sys
+
+Arg = sys.argv[:]
+bubble = set()
+with open(Arg[1], 'r') as f:
+    for line in f:
+        bubble.add(line[:-1])
+with open(Arg[2], 'r') as f:
+    for line in f:
+        if line[:-1] in bubble:
+            bubble.remove(line[:-1])
+with open(Arg[3], 'r') as f:
+    for line in f:
+        if line[:-1] in bubble:
+            bubble.remove(line[:-1])
+with open(Arg[4], 'r') as f:
+    for line in f:
+        if line[:-1] in bubble:
+            bubble.remove(line[:-1])
+for el in bubble:
+    print(el)
