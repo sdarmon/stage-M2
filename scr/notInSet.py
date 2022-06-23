a7a47999738326ce92e6f0760a09b2ec2addab3f
sasha Darmon <sasha.darmon@ens-lyon.fr>
2022-06-23 10:32:30 +0200

    fonction notInSet

--- scr/notInSet.py
+++ scr/notInSet.py
@@ -2,10 +2,12 @@ import sys
 
 Arg = sys.argv[:]
 bubble = set()
+correspondance = dict()
 with open(Arg[1], 'r') as f:
     for line in f:
         L=line.split('\t')
         bubble.add(L[-4])
+        correspondance[bubble]=line[:-1]
 with open(Arg[2], 'r') as f:
     for line in f:
         b=line.split('\t')[-1][:-1]
@@ -22,4 +24,4 @@ with open(Arg[4], 'r') as f:
         if b in bubble:
             bubble.remove(b)
 for el in bubble:
-    print(el)
+    print(correspondance[el])
