b3c7b19d4d556407ee9ca68b5b1c00e2b399ebda
sasha Darmon <sasha.darmon@ens-lyon.fr>
2022-06-27 09:55:32 +0200

    Traduction de plot

--- scr/plot.py
+++ scr/plot.py
@@ -48,13 +48,13 @@ elif Arg[2] == "dot":
             yg.append(Y[i])
             xg.append(x[i])
 
-    fig, axs = plt.subplots(2)
+    #fig, axs = plt.subplots(1)
 
-    axs[0].plot(xg, yg, 'bo', label='Fréquence non nulle')
-    axs[0].plot(x, Y, 'green')
-    axs[0].ylabel("Frequences (en %)")
-    axs[0].xlabel("Taille des poids")
-    axs[0].suptitle("Fréquence des différents poids (Pour un rayon de 10 nucléotides)")#fig.suptitle("Frequencies of the different sizes of weight (Case of " + Arg[0][:-4] + ")")
+    plt.plot(xg, yg, 'bo', label='Fréquence non nulle')
+    plt.plot(x, Y, 'green')
+    plt.ylabel("Frequences (en %)")
+    plt.xlabel("Taille des poids")
+    plt.title("Fréquence des différents poids (Pour un rayon de 10 nucléotides)")#fig.suptitle("Frequencies of the different sizes of weight (Case of " + Arg[0][:-4] + ")")
 
 
     #axs[1].plot(xb, yb, 'ro', label='Null Frequency')
