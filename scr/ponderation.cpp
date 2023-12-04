8bc3796e8f711975eceeea4cbd11be3d24ed2517
sdarmon <sasha.darmon@univ-lyon1.fr>
2023-12-04 16:53:24 +0100

    adding droso

--- scr/ponderation.cpp
+++ scr/ponderation.cpp
@@ -47,6 +47,7 @@ int main(int argc, char** argv)
         printGraphVertices(G,output);
         output.close();
     } else if(argc == 8 and argv[6][1]=='o'){
+        cout << "ok here?" << endl;
         ofstream output;
         output.open(argv[7]);
         printGraphVertices(G,output);
