1ba72c6e2066fb5eb9c8a7651b60a50ed5e07f2d
sdarmon <sasha.darmon@univ-lyon1.fr>
2024-03-20 10:41:37 +0100

    HC

--- scr/homomorphic_compression.py
+++ scr/homomorphic_compression.py
@@ -2,19 +2,22 @@ import sys
 
 Arg = sys.argv[:]
 
-def homomorphic_compression(read):
-    #Removing consecutive duplicates characters
-    S = read[0]
-    for i in range(1, len(read)):
-        if read[i] != read[i-1]:
-            S += read[i]
+def homomorphic_compression(read,w):
+    #Removing consecutive duplicates window by window
+    S = read[0:w]
+    current_w = read[0:w]
+    for i in range(1, len(read)-w):
+        next_w = read[i:i+w]
+        if current_w != next_w:
+            S += read[i+w-1]
     return S
 
-if len(Arg) not in [3]:
-    print("Use : " + Arg[0] + " reads.fq reads_out.fq")
+if len(Arg) not in [4]:
+    print("Use : " + Arg[0] + " reads.fq reads_out.fq window")
     exit()
 
 cycle = 0
+w = int(Arg[3])
 if len(Arg) == 3:
     with open(Arg[1], 'r') as f:
         with open(Arg[2], 'w') as f_out:
@@ -24,4 +27,4 @@ if len(Arg) == 3:
                     if cycle != 2:
                         f_out.write(line)
                         continue
-                    f_out.write(homomorphic_compression(line))
+                    f_out.write(homomorphic_compression(line,w))
